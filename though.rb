583e1a0ccd73cab3d5bbeafc223f8aa352e89d59
locham93 <aniladhokoiyag@hotmail.com>
2025-03-26 15:35:03 +0800

    style(core): format though.rb code style

--- though.rb
+++ though.rb
@@ -1,1 +1,20 @@
-puts "Hello, World!"
+#!/usr/bin/env ruby
+# Though - A simple Ruby program
+
+module Though
+  class Greeter
+    def initialize(name = "World")
+      @name = name
+    end
+
+    def greet
+      puts "Hello, #{@name}!"
+    end
+  end
+end
+
+# Main execution
+if __FILE__ == $0
+  greeter = Though::Greeter.new
+  greeter.greet
+end
